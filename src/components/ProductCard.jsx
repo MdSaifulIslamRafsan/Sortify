bd7b7b4cc76a05a98c8623e1f8d3cbd04b9e0ecf
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2024-11-18 18:22:19 +0600

    added product details modal

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,24 +1,56 @@
+/* eslint-disable react/prop-types */
 
-const ProductCard = ({product}) => {
-    const {id , title , price , description , category , image, rating} = product
-    return (
-        <div className="card bg-base-300 shadow-xl">
-  <figure>
-    <img
-    className="h-72 rounded-2xl p-2 w-full"
-      src={image}
-      alt={title} />
-  </figure>
-  <div className="card-body">
-    <h2 className="card-title">{title}</h2>
-     <span className="">Price: ${price}</span>
-     <p className="">Category: {category}</p>
-    <div className="card-actions justify-center">
-      <button className="btn btn-primary">View Details</button>
+
+const ProductCard = ({ product }) => {
+  const { id, title, price, description, category, image, rating } = product;
+const handleModal = (id) =>{
+    document.getElementById(`modal_${id}`).showModal() 
+}
+  return (
+    <div className="card bg-base-300 shadow-xl">
+      <figure>
+        <img className="h-72 rounded-2xl p-2 w-full" src={image} alt={title} />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{title}</h2>
+        <span className="">Price: ${price}</span>
+        <p className="">Category: {category}</p>
+        <div className="card-actions justify-center">
+          <button
+            className="btn btn-primary"
+            onClick={() => handleModal(id)}
+          >
+            View Details
+          </button>
+        </div>
+
+        {
+          <dialog id={`modal_${id}`} className="modal">
+            <div className="modal-box">
+              <img
+                className="h-72 rounded-2xl p-2 w-full"
+                src={image}
+                alt={title}
+              />
+              <h3 className="font-bold text-lg">{title}</h3>
+              <p className="py-4">{description}</p>
+              <div className="md:flex md:justify-between">
+                <p className="!grow-0">Price: ${price}</p>
+                <p className="!grow-0">Category: {category}</p>
+              </div>
+              <div className="md:flex md:justify-between">
+                <p className="!grow-0">rating: {rating?.rate}</p>
+                <p className="!grow-0">count: {rating?.count}</p>
+              </div>
+            </div>
+            <form method="dialog" className="modal-backdrop">
+              <button>close</button>
+            </form>
+          </dialog>
+        }
+      </div>
     </div>
-  </div>
-</div>
-    );
+  );
 };
 
 export default ProductCard;
