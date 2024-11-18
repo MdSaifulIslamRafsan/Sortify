60bdab9ece293dbf281cf1a13feb263ea7841baf
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2024-11-18 17:07:59 +0600

    added loading spinner

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -2,20 +2,27 @@ import { useEffect, useState } from "react";
 
 const Products = () => {
   const [productsData, setProductsData] = useState([]);
- 
-  
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch("https://fakestoreapi.com/products")
       .then((res) => res.json())
       .then((data) => {
         setProductsData(data);
-       
+        setLoading(false);
       });
   }, []);
 
- 
-  return <div></div>;
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <span className="loading loading-spinner loading-lg"></span>
+      </div>
+    );
+  }
+  return <div>
+    
+  </div>;
 };
 
 export default Products;
